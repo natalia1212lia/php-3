f5625f7362b0c38378737200454727a80eaad59e
pinomanulang <pinokristantim@gmail.com>
2020-07-30 07:41:55 +0700

    penambahan file skor-terbesar.php dan cari-mean.php

--- skor-terbesar.php
+++ skor-terbesar.php
@@ -1,7 +1,44 @@
 <?php
-function skor_terbesar($arr){
+function skor_terbesar($data){
 //kode di sini
+  $result =[];
+  $kelasReactNative=[];
+  $kelasReactjs=[];
+  $kelaslaravel=[];
+  foreach ($data as $key => $row) {
+    # code...
+    $nilai[$key] = $row ['nilai'];
+    $kelas[$key] = $row ['kelas'];
+  }
+  array_multisort($kelas, SORT_DESC,$nilai,SORT_DESC,$data);
+  for ($i=0; $i <count($data) ; $i++) { 
+    # code...
+    if ($data[$i]['kelas']=='React Native') {
+      $kelasReactNative[]=$data[$i];
+      # code...
+    }elseif ($data[$i]['kelas']=='React JS') {
+      # code...
+       $kelasReactjs[]=$data[$i];
+    }elseif ($data[$i]['kelas']=='Laravel') {
+      # code...
+       $kelaslaravel[]=$data[$i];
+    }
+  }
+  
+print_r($kelasReactNative);
+print_r($kelasReactjs);
+print_r($kelaslaravel);
+
+$result[]=$kelasReactNative[0];
+$result[]=$kelasReactjs[0];
+$result[]=$kelaslaravel[0];
+foreach ($result as $key => $hasil) {
+  echo "Skor Terbesar"." ".$hasil['kelas']." "."diperoleh oleh"." "." ".$hasil['nilai']."<br>";
+  # code...
 }
+
+}
+//print_r($nilai);
 
 // TEST CASES
 $skor = [
